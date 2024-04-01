c33ea01c1619753fda508c891056dc7fef2e6dc9
Oscar Nava <onava@causanatura.org>
2024-04-01 12:20:22 -0600

    File to create dataset from M2 and weather data

---
+++ Predictor/Data Validation/prpi_datavalidation/create_dataset.py
@@ -0,0 +1,5 @@
+#******************** AQUÍ SE CREA EL DATASET DE IRREGULARIDADES ********************
+#%%     SE IMOPRTAN LAS LIBRERIAS NECESARIAS
+import pandas as pd
+import geopandas as gpd
+import os
