8e40971d92e1f7ba1cdd60a2c1b5161badf68d6c
paulinaloza <paulina.loza@designamx.com>
2024-06-06 12:38:43 -0600

    fix: CU-190 eliminar lineas app router

--- ui/src/router/AppRouter.tsx
+++ ui/src/router/AppRouter.tsx
@@ -3,14 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { paths } from 'constants/paths';
 import LandingPage from 'pages/LandingPage';
 import UploadErrorsPage from 'pages/UploadErrorsPage';
-import { AdminLoginPage } from 'pages/auth/AdminLoginPage';
-import { ForgotPasswordPage } from 'pages/auth/ForgotPasswordPage';
 import RequireAuth from './RequireAuth';
 import AuthGuard from './AuthGuard';
 import AdminLayout from 'components/layouts/AdminLayout';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { UnauthorizedPage } from 'pages/UnauthorizedPage';
-import { SetPasswordPage } from 'pages/auth/SetPasswordPage';
 import SimpleHeaderLayout from 'components/layouts/SimpleHeaderLayout';
 import Logout from 'pages/auth/Logout';
 import 'styles/theme-globals.scss';
@@ -31,20 +28,9 @@ export const AppRouter = () => {
       <Routes>
         <Route path={paths.global.index} element={<MainLayout />}>
           <Route index element={<LandingPage />} />
-          <Route path={paths.login.index}>
-            <Route path={paths.login.admin} element={<AdminLoginPage />} />
-          </Route>
           <Route path={paths.forgotPassword.index}>
-            <Route
-              path={paths.forgotPassword.admin}
-              element={<ForgotPasswordPage />}
-            />
           </Route>
           <Route path={paths.auth.index}>
-            <Route
-              path={`${paths.admin.index}/${paths.auth.confirm}`}
-              element={<SetPasswordPage />}
-            />
             <Route path={paths.auth.logout} element={<Logout />} />
           </Route>
         </Route>
