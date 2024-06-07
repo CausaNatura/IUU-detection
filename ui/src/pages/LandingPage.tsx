83cf1a05991b9ff520b3f8ce45cdf681b9e34577
paulinaloza <paulina.loza@designamx.com>
2024-06-07 14:22:01 -0600

    feat: CN-187 feature message component

--- ui/src/pages/LandingPage.tsx
+++ ui/src/pages/LandingPage.tsx
@@ -2,17 +2,19 @@ import { Col, Row } from 'antd';
 import 'styles/global.scss';
 import { PageTitle } from 'components/common/PageTitle';
 import Scale from 'components/common/Scale';
+import Zone from 'components/common/Zone';
+import FeatureMessage from 'components/common/FeatureMessage';
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <PageTitle>Predictor de Riesgos | </PageTitle>
       <Row>
-        <Col xs={16}>
-          .
-        </Col>
-        <Col className='g-left-container' xs={8}>
-          <Scale/>
+        <Col xs={16}>.</Col>
+        <Col className="g-left-container" xs={8}>
+          <Scale />
+          <Zone />
+          <FeatureMessage />
         </Col>
       </Row>
     </div>
