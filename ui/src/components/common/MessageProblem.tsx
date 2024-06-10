6ddc1c41d81b252e8a971255de3c6198191300c4
paulinaloza <paulina.loza@designamx.com>
2024-06-10 16:53:09 -0600

    fix: CN-188 estilos popconfirm

--- ui/src/components/common/MessageProblem.tsx
+++ ui/src/components/common/MessageProblem.tsx
@@ -1,22 +1,33 @@
 import { Button, Image, Popconfirm } from 'antd';
 import '../../styles/messages.scss';
 import MessageReport from '/assets/icon/icon-message-report.svg';
+import Copy from '/assets/icon/icon-copy.svg';
+import Link from 'antd/es/typography/Link';
 
 const MessageProblem = () => {
   return (
     <div className="message-problem-container">
       <Popconfirm
         title={
-          <h2>¿Algún problema para visualizar correctamente el predictor en tu zona?</h2>
+          <h2>
+            ¿Algún problema para visualizar correctamente el predictor en tu
+            zona?
+          </h2>
         }
         description={
           <p>
             Envía un correo con el asunto
-            <span className='subject'> Error predictor datacenter</span>
-            <span className='mail'> info@causanatura.org</span> o da click aquí para conectar con
-            tu cuenta de email de forma automática
+            <span className="subject"> Error predictor datacenter</span>
+            <Link href="" className="mail">
+              {' '}
+              info@causanatura.org <Image src={Copy} preview={false} />
+            </Link>{' '}
+            O da <Link className="link">click aquí</Link> para conectar con tu
+            cuenta de email de forma automática
           </p>
         }
+        showCancel={false}
+        okText="x"
       >
         <Button className="btn-info" href="#">
           <Image src={MessageReport} preview={false} />
