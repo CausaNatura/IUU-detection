89274008713e8f16e7b35e5b5dc6b120b4be5902
paulinaloza <paulina.loza@designamx.com>
2024-06-10 17:17:26 -0600

    feat: CN-188 copy to clipboard

--- ui/src/components/common/MessageProblem.tsx
+++ ui/src/components/common/MessageProblem.tsx
@@ -3,8 +3,19 @@ import '../../styles/messages.scss';
 import MessageReport from '/assets/icon/icon-message-report.svg';
 import Copy from '/assets/icon/icon-copy.svg';
 import Link from 'antd/es/typography/Link';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useState } from 'react';
 
 const MessageProblem = () => {
+  const [copyStatus, setCopyStatus] = useState(false); 
+  
+  const onCopyText = () => {
+
+    setCopyStatus(true);
+
+    setTimeout(() => setCopyStatus(false), 3000); 
+  };
+  
   return (
     <div className="message-problem-container">
       <Popconfirm
@@ -15,16 +26,23 @@ const MessageProblem = () => {
           </h2>
         }
         description={
-          <p>
-            Envía un correo con el asunto
-            <span className="subject"> Error predictor datacenter</span>
-            <Link href="" className="mail">
-              {' '}
-              info@causanatura.org <Image src={Copy} preview={false} />
-            </Link>{' '}
-            O da <Link className="link">click aquí</Link> para conectar con tu
-            cuenta de email de forma automática
-          </p>
+          <>
+            <p>
+              Envía un correo con el asunto
+              <span className="subject"> Error predictor datacenter</span></p>
+              <p>
+                <CopyToClipboard text='info@causanatura.org' onCopy={onCopyText}>
+                  <div className="mail">
+                      info@causanatura.org <Image src={Copy} preview={false} />
+                  </div>
+                </CopyToClipboard>
+                {copyStatus && <div className='copied'>Se ha copiado el email en el portapapeles</div>}
+              </p>
+            <p>
+                O da <Link className="link">click aquí</Link> para conectar con tu
+              cuenta de email de forma automática
+            </p>
+          </>
         }
         showCancel={false}
         okText="x"
