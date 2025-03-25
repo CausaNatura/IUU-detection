3a3698fb329a610d7b96fe90d32aae017e0b4d46
Oscar Nava <onava@causanatura.org>
2025-03-25 12:10:54 -0600

    Updates

--- Predictor/Application/API/api-predictor/app/main.py
+++ Predictor/Application/API/api-predictor/app/main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import predict_corridor
 
-app = FastAPI(openapi_prefix="/api-predictor")
+app = FastAPI()
 
 # Configuración de CORS
-app.add_middleware(
+'''app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Puedes especificar dominios en lugar de "*"
     allow_credentials=True,
     allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Permite todos los headers
-)
+)'''
 
 @app.get("/")
 def read_root():
